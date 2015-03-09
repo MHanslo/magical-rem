546baedd9f6292a1798577c2fbce2d68f2e7e2ae
michael.hanslo <michael.hanslo@ogilvy.co.za>
2015-03-09 13:37:51 +0200

    First pages commit

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  nebula = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  progress = require('bespoke-progress');
+
+// Bespoke.js
+bespoke.from('article', [
+  nebula(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  progress()
+]);
